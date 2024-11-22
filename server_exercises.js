1189d23c56831cdb7469a0247813b00bd14c2f03
riikkana <naamanka.riikka@gmail.com>
2024-11-22 12:24:02 +0200

    first endpoints, work in progress

--- server_exercises.js
+++ server_exercises.js
@@ -1,5 +1,3 @@
-// work in progress
-
 import express from 'express';
 
 let app = express();
@@ -14,33 +12,7 @@ app.listen(port, () => {
     console.log('The server is running');
 });
 
-//  all users
-app.get('/user', (req, res) => {
-  res.json(user);
-});
-
-// user by id
-app.get('/user/:id', (req, res) => {
-  res.json(user);
-});
-
-// POST user with body parameters
-app.post('/user', (req, res) => {
-  console.log(req.body);
-  res.send();
-});
-
-// movies by genre
-app.get('/movie/:genre', (req, res) => {
-  res.json(user);
-});
-
-// reviews by users
-app.get('/review/:username', (req, res) => {
-  res.json(user);
-});
-
-/* //Defining an endpoint
+//Defining an endpoint
 app.get('/', (req, res) => {
     res.send('<h1>Hello!</h1>');
 })
@@ -55,17 +27,22 @@ app.get('/text', (req, res) => {
     const person = { name: "John Doe", age: 30 };
     res.json(person);
   });
-   */
-
-/* // Endpoint that returns (these are path parameters)
+  
+// Endpoint that returns (these are path parameters)
 // url in browser: localhost:3001/user/fname/lname
-app.get('/user/:fname/:lname', (req, res) => {
-  const fname = req.params['fname'];
-  const lname = req.params['lname'];
-  res.send(`You requested ${fname} ${lname}`)
-  }); */
+  app.get('/user/:fname/:lname', (req, res) => {
+    const fname = req.params['fname'];
+    const lname = req.params['lname'];
+    res.send(`You requested ${fname} ${lname}`)
+    });
 
-/*   // Endpoint that returns (these are query parameters)
+// POST user with body parameters
+  app.post('/user', (req, res) => {
+    console.log(req.body);
+    res.send();
+  });
+
+  // Endpoint that returns (these are query parameters)
   // url in browser: localhost:3001/user?username=nimi&age=15
   app.get('/info', (req, res) => {
     const user = req.query.username;
@@ -74,7 +51,7 @@ app.get('/user/:fname/:lname', (req, res) => {
       res.status(400).send(`Age can't be zero!`);
     } else {
       res.send(`You requested ${user}, ${age}`)
-  }}); */
+  }});
 
   // Endpoint that returns an array of objects
 app.get('/array', (req, res) => {
