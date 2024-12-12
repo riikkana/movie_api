35f83edac8f48d3e229422d9e6aef11a67e3c901
riikkana <naamanka.riikka@gmail.com>
2024-12-12 09:24:51 +0200

    endpoints 6/10 done

--- db_queries.js
+++ db_queries.js
@@ -1,20 +1,31 @@
 import { pgPool } from "./pg_connection.js";
 
 const SQL = {
-    GET_ALL_EMPLOYEES: 'SELECT * FROM movie',
-    GET_EMPLOYEES: 'SELECT * FROM movie WHERE name LIKE $1',
-    GET_MOVIE_USERS: 'SELECT * FROM movie_user'
+    GET_ALL_MOVIES: 'SELECT * FROM movie',
+    GET_MOVIES: 'SELECT * FROM movie WHERE name LIKE $1',
+    GET_MOVIES_BY_ID: 'SELECT * FROM movie WHERE id=$1',
+    GET_MOVIE_USERS: 'SELECT * FROM movie_user',
+    //ADD_MOVIES: 'INSERT INTO movie VALUES ($1,$2,$3,$4)'
 }
 
 async function getMovies(keyword) {
     try {
         let result;
         if(!keyword) {
-            result = await pgPool.query(SQL.GET_ALL_EMPLOYEES);
+            result = await pgPool.query(SQL.GET_ALL_MOVIES);
         } else {
             keyword =  '%'+keyword+'%';
-            result = await pgPool.query(SQL.GET_EMPLOYEES, [keyword]);
+            result = await pgPool.query(SQL.GET_MOVIES, [keyword]);
         }
+        return result.rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function getMoviesById(id) {
+    try {
+        const result = await pgPool.query(SQL.GET_MOVIES_BY_ID, [id]);
         return result.rows;
     } catch (error) {
         throw error;
@@ -29,4 +40,14 @@ async function getMovieUsers() {
         throw error;
     }
 }
-export {getMovies, getMovieUsers};
+
+/*async function addMovies(id, name, year, genre) {
+    try {
+        const result = await pgPool.query(SQL.ADD_MOVIES, [id, name, year, genre]);
+        return result;
+    } catch (error) {
+        throw error;
+    }
+}*/
+
+export {getMovies, getMovieUsers, getMoviesById};
--- server.js
+++ server.js
@@ -1,17 +1,75 @@
 import express from 'express';
 import { pgPool } from './pg_connection.js';
-import { getMovies, getMovieUsers } from './db_queries.js';
+import { getMovies, getMoviesById, getMovieUsers } from './db_queries.js';
 
 let app = express();
 
 const port = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.listen(port, () => {
     console.log('The server is running');
 });
 
+//1. add new genre
+app.post('/genre', async (req,res) => {
+    const id = req.body.id;
+    const description = req.body.description;
+    try {
+        const result = await pgPool.query(
+            'INSERT INTO genre VALUES ($1,$2)', [id, description]);
+        res.end();
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+});
+
+//2. add new movie
+app.post('/movie', async (req,res) => {
+    const id = req.body.id;
+    const name = req.body.name;
+    const year = req.body.year;
+    const genre = req.body.genre;
+    try {
+        const result = await pgPool.query(
+            'INSERT INTO movie VALUES ($1,$2,$3, $4)', [id, name, year, genre]);
+        res.end();
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+});
+
+//3. add new user
+app.post('/movie_user', async (req,res) => {
+    const id = req.body.id;
+    const uname = req.body.uname;
+    const name = req.body.name;
+    const pword = req.body.pword;
+    const yob = req.body.yob;
+    try {
+        const result = await pgPool.query(
+            'INSERT INTO movie_user VALUES ($1,$2,$3,$4,$5)', [id, uname, name, pword, yob]);
+        res.end();
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+});
+
+//4. get movies by id
+app.get('/movies_id', async (req,res) => {
+    let id = req.query.id;
+    try {
+        res.json(await getMoviesById(id));
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+});
+
+//5. REMOVE MOVIE BY ID PUUTTUU VIELÄ
+
+//6. ja 7. get movies; all and by keyword
 app.get('/movies', async (req,res) => {
     let keyword = req.query.keyword;
     try {
@@ -21,6 +79,12 @@ app.get('/movies', async (req,res) => {
     }
 });
 
+//8. ADD MOVIE REVIEW PUUTTUU VIELÄ
+//9. ADD FAVORITE MOVIE FOR USER PUUTTUU VIELÄ
+//10. GET FAVORITE MOVIES BY USERNAME PUUTTUU VIELÄ
+
+
+//get all users, ei ollut tehtävänannossa
  app.get('/movie_users', async (req,res) => {
     try {
         res.json(await getMovieUsers());
@@ -29,17 +93,3 @@ app.get('/movies', async (req,res) => {
     }
 });
 
- app.post('/movie', async (req,res) => {
-    const id = req.body.mid;
-    const name = req.body.mname;
-    const year = req.body.myear;
-    const genre = req.body.genre_id;
-
-    try {
-        const result = await pgPool.query(
-            'INSERT INTO movie VALUES ($1,$2,$3,$4)', [id, name, year, genre]);
-        res.end();
-    } catch (error) {
-        res.status(400).json({error: error.message});
-    }
-});
